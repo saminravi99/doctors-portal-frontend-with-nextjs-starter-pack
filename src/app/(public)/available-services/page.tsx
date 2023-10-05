5b7363b734521183e62b023596139bb68193875c
Anisur Rahman <anisurrahman06046@gmail.com>
2023-10-05 21:24:10 +0600

    ssr

--- src/app/(public)/available-services/page.tsx
+++ src/app/(public)/available-services/page.tsx
@@ -1,9 +1,10 @@
-import React from 'react'
+const AvailableServicePublic = async () => {
+  const res = await fetch("http://localhost:5000/api/v1/available-services", {
+    cache: "no-store",
+  });
+  const { data } = await res.json();
+  console.log(data);
+  return <div>AvailableServicePublic</div>;
+};
 
-const AvailableServicePublic = () => {
-  return (
-    <div>AvailableServicePublic</div>
-  )
-}
-
-export default AvailableServicePublic
+export default AvailableServicePublic;
